064168c98f5a51bc2efe273ff6ccc5e9f9bd8652
tiger94un <lowwaels98@gmail.com>
2024-10-14 12:58:57 +0300

    just made smoth camera

--- for player/gdscript/gdscript for player/player.gd
+++ for player/gdscript/gdscript for player/player.gd
@@ -2,13 +2,17 @@ extends CharacterBody3D
 
 
 @onready var head: Node3D = $head
-@onready var eyes: Camera3D = $head/Camera3D
 
-var speed_of_look := 0.04
+@onready var eyes: Camera3D = $head/movelikesholder/Camera3D
 
-var speed = 50.0
+@onready var movelikesholder: Node3D = $head/movelikesholder
 
-const JUMP_VELOCITY = 40.5
+var speed_of_look := 0.03
+
+
+var speed = 5.0
+
+const JUMP_VELOCITY = 4.5
 
 var is_runing : bool = true
 
@@ -18,18 +22,18 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * speed_of_look)
+		movelikesholder.rotate_y(-event.relative.x * speed_of_look)
 		eyes.rotate_x(-event.relative.y * speed_of_look)
-		eyes.rotation.x = clamp(eyes.rotation.x, deg_to_rad(-45), deg_to_rad(100))
+		eyes.rotation.x = clamp(eyes.rotation.x, deg_to_rad(-85), deg_to_rad(100))
 
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	if Input.is_action_just_pressed("run"):
@@ -38,14 +42,14 @@ func _physics_process(delta: float) -> void:
 			is_runing  = false
 			print("sspeed iss 70")
 		else:
-			speed = 50.0
+			speed = 5.0
 			is_runing = true
-			print("speed iss 50")
+			print("speed iss 5")
 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
@@ -55,3 +59,6 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+
+	movelikesholder.global_position = head.global_position
+	head.global_rotation = movelikesholder.global_rotation

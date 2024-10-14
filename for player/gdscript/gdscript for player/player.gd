b25b639c657473d9711a9ba15f14a7fdbd35b3cc
tiger94un <lowwaels98@gmail.com>
2024-10-14 09:01:57 +0300

    it make run and make light

--- for player/gdscript/gdscript for player/player.gd
+++ for player/gdscript/gdscript for player/player.gd
@@ -1,19 +1,27 @@
 extends CharacterBody3D
 
+
 @onready var head: Node3D = $head
 @onready var eyes: Camera3D = $head/Camera3D
+
 var speed_of_look := 0.04
-const SPEED = 50.0
+
+var speed = 50.0
+
 const JUMP_VELOCITY = 40.5
+
+var is_runing : bool = true
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * speed_of_look)
 		eyes.rotate_x(-event.relative.y * speed_of_look)
 		eyes.rotation.x = clamp(eyes.rotation.x, deg_to_rad(-45), deg_to_rad(100))
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -24,15 +32,26 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_pressed("run"):
+		if is_runing:
+			speed = 70.0
+			is_runing  = false
+			print("sspeed iss 70")
+		else:
+			speed = 50.0
+			is_runing = true
+			print("speed iss 50")
+
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
